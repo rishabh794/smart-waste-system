d8ba2dce5518832e3fce898b5a6e728922314da3
rishabh794 <rishabhbuchha123456789@gmail.com>
2026-06-18 12:47:48 +0530

    fix: trust proxy to resolve express-rate-limit error on render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1); // Trust first proxy (needed for Render & express-rate-limit)
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
